9e77ec8b9c10606a9ed92cdcb39e1238e036278f
tuanla99 <luonganhtuan1051999@gmail.com>
2019-12-13 17:31:47 +0700

    Database

---
+++ Query.sql
@@ -0,0 +1,50 @@
+
+
+use master
+go
+drop database UNIFY
+use master
+go
+create database UNIFY
+go
+use UNIFY
+go
+
+
+create table [User](
+	id int identity,
+	email varchar(50),
+	username varchar(50) unique not null,
+	[password] varchar(36) not null,
+	avatar varchar(50),
+	role_id int,
+	primary key(id)
+)
+create table Category(
+	cate_id int identity primary key,
+	cate_name nvarchar(255) not null,
+)
+
+create table Product(
+	id int identity primary key,
+	[name] nvarchar(255) not null,
+	price float ,
+	cate_id int references Category(cate_id) ,
+	des varchar(2000),
+	image varchar(50),
+)
+
+
+create table Cart(
+	id varchar(50) primary key,
+	u_id int references [User](id)  ,
+	buyDate date
+)
+create table CartItem(
+	id varchar(50) primary key,
+	quantity int,
+	unitPrice float,
+	pro_id int references Product(id) ,
+	cat_id varchar(50) references Cart(id)
+)
+
